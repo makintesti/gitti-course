f4b6b56822c30c9252c96b9da80da965560dbab1
makinak <aki.makinen@tuni.fi>
2025-09-08 23:52:47 +0300

    add prints

--- hello_world.py
+++ hello_world.py
@@ -2,4 +2,13 @@
 print("Hello World!")
 =======
 print("Hello world!")
+<<<<<<< HEAD
 >>>>>>> uusitest/master
+=======
+print("Not another hello world")
+print("No")
+print("More")
+print("Hello")
+print("World")
+print("Please, I'm begging you")
+>>>>>>> 0eb43c0 (add prints)
